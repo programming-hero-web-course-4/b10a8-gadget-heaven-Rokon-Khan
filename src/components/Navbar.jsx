1b30de967f7b45c9cfd8e2ef8c23378154d0737d
Rokon Khan <rokonjust@gmail.com>
2024-11-03 23:19:38 +0600

    Hero Section Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -22,36 +23,23 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        <Link to="/">Home</Link>
+        <Link to="/Statistics">Statistics</Link>
+        <Link to="/Dashboard">Dashboard</Link>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Gadget Heaven</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+    <ul className="menu menu-horizontal px-1 gap-6">
+        <Link to="/">Home</Link>
+        <Link to="/Statistics">Statistics</Link>
+        <Link to="/Dashboard">Dashboard</Link>
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
+  <div className="navbar-end gap-5">
+    <i className="fa-solid fa-cart-shopping text-2xl"></i>
+    <i className="fa-regular fa-heart text-2xl"></i>
   </div>
 </div> 
         </div>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
 const MainLaout = () => {
@@ -7,8 +8,9 @@ const MainLaout = () => {
         <div>
             {/* Navbar */}
             <Navbar></Navbar>
-            
-            <div className="min-h-[calc(100vh-300px)] py-12 container mx-auto px-12">
+            {/* Hero */}
+            <Hero> </Hero>
+            <div className="min-h-[calc(100vh-300px)]">
             {/* Dynamic Contents */}
             <Outlet></Outlet>
             </div>
