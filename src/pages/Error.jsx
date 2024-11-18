3e463d349882804274b90e7f98250ec7ca7dbd59
Md Rokonuzzaman <rokonjust@gmail.com>
2024-11-18 16:31:16 +0600

    404 Page Button Added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,13 @@
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 import error from "../assets/404.gif";
 const Error = () => {
   return (
-    <div>
+    <div className="min-h-screen flex justify-center items-center flex-col">
       <div className="max-w-[1024px] h-full mx-auto">
         <img className="w-ful h-full" src={error} alt="error" />
       </div>
+      <div><Link to="/" className="btn bg-[#9538E2] text-white tex-xl font-bold hover:text-[#00be6c]"> <FaArrowLeftLong /> Return Home</Link></div>
     </div>
   );
 };
